a40771153f2e3efe71492f90efb1fb356c3fbd0d
Mario Esho <mario.esho@outlook.com>
2022-07-16 19:40:28 -0400

    added super mario 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: AppComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    PageNotFoundComponent
+  ],
+  imports: [ ],
+  exports: [ ]
+})
+export class ComponentsModule { }
