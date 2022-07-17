a1f763eb2b1b57f16392394bd95a461bb460d4c6
Mario Esho <mario.esho@outlook.com>
2022-07-16 20:22:53 -0400

    add zion womens route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { ZionWomensSoccerComponent } from './teams/zion-womens-soccer/zion-womens-soccer.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: AppComponent },
+  { path: 'zion-womens-soccer', component: ZionWomensSoccerComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
