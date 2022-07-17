f348d2f0b0de38ad2077712f0debecc867d91f94
Mario Esho <mario.esho@outlook.com>
2022-07-16 22:22:23 -0400

    add link component (#6)

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LinkComponent } from './link/link.component';
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LinkComponent
   ],
-  imports: [ ],
-  exports: [ ]
+  imports: [CommonModule, RouterModule],
+  exports: [LinkComponent]
 })
 export class ComponentsModule { }
