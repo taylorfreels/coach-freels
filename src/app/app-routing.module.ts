52d6c1976c883a3065fe7a6a359417b06ffc2c1a
Mario Esho <mario.esho@outlook.com>
2022-07-16 21:08:32 -0400

    Add teams pages (#3)
    * add teams module
    
    * add zion womens route
    
    * downgrade to angular 13 and add bootstrap MD5
    
    * add md5 dependencies
    
    * add datatable for team

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { ZionWomensSoccerComponent } from './teams/zion-womens-soccer/zion-womens-soccer.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: AppComponent },
+  { path: '', pathMatch: 'full', component: PageNotFoundComponent },
+  { path: 'trf', component: AppComponent },
+  { path: 'zion-womens-soccer', component: ZionWomensSoccerComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
---
+++ src/app/teams/teams.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ZionWomensSoccerComponent } from './zion-womens-soccer/zion-womens-soccer.component';
+
+
+
+@NgModule({
+  declarations: [
+    ZionWomensSoccerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeamsModule { }
