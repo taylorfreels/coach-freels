fc10082d9a43218eba37e96835cbea33303466b6
Mario Esho <mario.esho@outlook.com>
2022-07-16 22:01:32 -0400

    add taylor's page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { TrfComponent } from './trf/trf.component';
 import { ColumbiaArsenalU17bSoccerComponent } from './teams/columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component';
 import { ZionMensSoccerComponent } from './teams/zion-mens-soccer/zion-mens-soccer.component';
 import { ZionWomensSoccerComponent } from './teams/zion-womens-soccer/zion-womens-soccer.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: PageNotFoundComponent },
-  { path: 'trf', component: AppComponent },
+  { path: 'trf3141592653589793', component: TrfComponent },
   { path: 'zion-womens-soccer', component: ZionWomensSoccerComponent },
   { path: 'zion-mens-soccer', component: ZionMensSoccerComponent },
   { path: 'columbia-arsenal-u17b-soccer', component: ColumbiaArsenalU17bSoccerComponent },
--- src/app/teams/teams.module.ts
+++ src/app/teams/teams.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ZionWomensSoccerComponent } from './zion-womens-soccer/zion-womens-soccer.component';
 import { ZionMensSoccerComponent } from './zion-mens-soccer/zion-mens-soccer.component';
 import { ColumbiaArsenalU17bSoccerComponent } from './columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component';
+import { TeamsComponent } from './teams.component';
+import { CoreModule } from '../core/core.module';
 
 
 
@@ -10,10 +12,10 @@ import { ColumbiaArsenalU17bSoccerComponent } from './columbia-arsenal-u17b-socc
   declarations: [
     ZionWomensSoccerComponent,
     ZionMensSoccerComponent,
-    ColumbiaArsenalU17bSoccerComponent
+    ColumbiaArsenalU17bSoccerComponent,
+    TeamsComponent
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CoreModule],
+  exports: [TeamsComponent]
 })
 export class TeamsModule { }
