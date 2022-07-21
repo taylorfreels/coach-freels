9c1e2ec5bd1f0e82c5717101564a00739f512d0d
Mario Esho <mario.esho@outlook.com>
2022-07-21 19:47:18 -0400

    add hidden route for practices

--- src/app/teams/columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component.ts
+++ src/app/teams/columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { hiddenRoute } from 'src/app/app-routing.module';
 import { Link } from '../../core/models/link';
 import { Game } from '../models/game';
 import { Practice } from '../models/practice';
@@ -14,14 +16,21 @@ export class ColumbiaArsenalU17bSoccerComponent implements OnInit {
     //new Game(new Date('7/3/2022'), 'Opponent 1', new Link('Google', 'https://www.google.com/')),
     //new Game(new Date('7/9/2022'), 'Opponent 2', new Link('Google', 'https://www.google.com/'))
   ];
-  public practices: Practice[] = [
-    //new Practice(new Date('7/4/2022'), new Link('Google', 'https://www.google.com/')),
-    //new Practice(new Date('7/10/2022'), new Link('Google', 'https://www.google.com/'))
+  public practices?: Practice[];
+
+  private openPractices: Practice[] = [
+    // new Practice(new Date('7/21/2022'), new Link('Test 2', ''))
+  ];
+  private closedPractices: Practice[] = [
+    ...this.openPractices,
+    // new Practice(new Date('7/20/2022'), new Link('Test 1', ''))
   ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.practices = this.router.url.includes(hiddenRoute) ? this.closedPractices : this.openPractices;
+    this.practices.sort((a, b) => b.date.getTime() - a.date.getTime());
+    this.games.sort((a, b) => b.date.getTime() - a.date.getTime());
   }
-
 }
