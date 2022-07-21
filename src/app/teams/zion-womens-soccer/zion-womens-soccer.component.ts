af7087bb17c8b9af930f2d7f98b3f2da5c9dbc15
Taylor Freels <taylorfreels4@gmail.com>
2022-07-21 17:39:15 -0500

    Zion WSoc Link Update

--- src/app/teams/zion-womens-soccer/zion-womens-soccer.component.ts
+++ src/app/teams/zion-womens-soccer/zion-womens-soccer.component.ts
@@ -11,12 +11,11 @@ import { Practice } from '../models/practice';
 })
 export class ZionWomensSoccerComponent implements OnInit {
   public games: Game[] = [
-    new Game(new Date('7/3/2022'), 'Opponent 1', new Link('Google', 'https://www.google.com/')),
-    new Game(new Date('7/9/2022'), 'Opponent 2', new Link('Google', 'https://www.google.com/'))
+    new Game(new Date('7/20/2022'), 'Zion vs Spring Hill (7v7)', new Link('VEO Link', 'https://app.veo.co/matches/20220721-072022-zion-vs-spring-hill/')),
+    new Game(new Date('7/20/2022'), 'Zion vs Shelbyville (7v7)', new Link('VEO Link', 'https://app.veo.co/matches/20220721-072022-zion-vs-shelbyville/')),
+    new Game(new Date('7/20/2022'), 'Zion vs Fairview (7v7)', new Link('VEO Link', 'https://app.veo.co/matches/20220721-072022-zion-vs-fairview/'))
   ];
   public practices: Practice[] = [
-    new Practice(new Date('7/4/2022'), new Link('Google', 'https://www.google.com/')),
-    new Practice(new Date('7/10/2022'), new Link('Google', 'https://www.google.com/'))
   ];
 
   constructor() { }
