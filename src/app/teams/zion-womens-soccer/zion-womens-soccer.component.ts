c2a000e365d5e66058c06b6264098796d92bf0b5
Taylor Freels <taylorfreels4@gmail.com>
2022-08-05 09:20:21 -0500

    add practice 08.04.22

--- src/app/teams/zion-womens-soccer/zion-womens-soccer.component.ts
+++ src/app/teams/zion-womens-soccer/zion-womens-soccer.component.ts
@@ -28,6 +28,7 @@ export class ZionWomensSoccerComponent implements OnInit {
   ];
   private closedPractices: Practice[] = [
     ...this.openPractices,
+    new Practice(new Date('8/4/2022'), new Link('https://app.veo.co/matches/20220804-080422-zion-wsoc-training/')),
     new Practice(new Date('8/3/2022'), new Link('https://app.veo.co/matches/20220803-080322-zion-wsoc-training/')),
     new Practice(new Date('8/2/2022'), new Link('https://app.veo.co/matches/20220802-zion-wsoc-training-080222/')),
     new Practice(new Date('7/29/2022'), new Link('https://app.veo.co/matches/20220729-072922-zion-wsoc-training/')),
