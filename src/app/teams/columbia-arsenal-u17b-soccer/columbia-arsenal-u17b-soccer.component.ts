e61db0e51669cee24967230ece351c64bcf50fc9
taylorfreels <109441335+taylorfreels@users.noreply.github.com>
2022-08-29 06:42:47 -0500

    add practice and game (#31)

--- src/app/teams/columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component.ts
+++ src/app/teams/columbia-arsenal-u17b-soccer/columbia-arsenal-u17b-soccer.component.ts
@@ -13,12 +13,13 @@ import { Practice } from '../models/practice';
 })
 export class ColumbiaArsenalU17bSoccerComponent implements OnInit {
   public games: Game[] = [
-    //new Game(new Date('7/3/2022'), 'Opponent 1', new Link('https://www.google.com/')),
+    new Game(new Date('8/28/2022'), 'Columbia Arsenal @ Padedores 06 Yellow', new Link('https://app.veo.co/matches/20220828-082822-columbia-arsenal-patedores-476d743e/')),
     //new Game(new Date('7/9/2022'), 'Opponent 2', new Link('https://www.google.com/'))
   ];
   public practices?: Practice[];
 
   private openPractices: Practice[] = [
+    new Practice(new Date('8/25/2022'), new Link('https://app.veo.co/matches/20220826-082522-columbia-arsenal-training-878e3dc6/')),
     new Practice(new Date('8/22/2022'), new Link('https://app.veo.co/matches/20220823-082222-columbia-arsenal-training-e905dd1c/')),
     new Practice(new Date('8/18/2022'), new Link('https://app.veo.co/matches/20220819-081822-columbia-arsenal-training-fec0e9fb/')),
     new Practice(new Date('8/8/2022'), new Link('https://app.veo.co/matches/20220809-080822-columbia-arsenal-training/')),
